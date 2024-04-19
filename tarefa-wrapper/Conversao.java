f301a56e43adf665905c9a8a8ad5fdeb595f0670
Tanous Azzi <tanous@protonmail.com>
2024-04-19 11:28:30 -0300

    Add exercício módulo 9

---
+++ tarefa-wrapper/Conversao.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Conversao {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double valor;
+        System.out.print("Digite um valor numerico: ");
+        valor = input.nextDouble();
+        Double n1 = valor;
+        System.out.println(n1);
+    }
+}
