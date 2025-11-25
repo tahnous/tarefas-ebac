20d8ebc8eeeacc8c2d5f0908ac884f19ca41a738
tahnous <tanous@protonmail.com>
2025-11-25 19:23:33 -0300

    Add exercício módulo 13

---
+++ mod13/PessoaJuridica.java
@@ -0,0 +1,12 @@
+public class PessoaJuridica extends Pessoa {
+
+    private String cnpj;
+
+    public String getCnpjj() {
+	return cpf;
+    }
+    public void setCnpj (String cnpj) {
+	this.cnpj = cnpj;
+    }
+
+}
